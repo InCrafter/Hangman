ef377d75fdd8b8a653bcb1c2a82f696b1090ef85
InCrafter <maxialvar07@gmail.com>
2022-08-18 14:44:58 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,178 @@
+const words = ["abruptly", "absurd", "abyss", "affix", "askew", "avenue", "awkward", "axiom", "azure", "bagpipes", "bandwagon", "banjo", "bayou", "beekeeper", "bikini", "blitz", "blizzard", "boggle", "bookworm", "boxcar", "boxful", "buckaroo", "buffalo", "buffoon", "buxom", "buzzard", "buzzing", "buzzwords", "caliph", "cobweb", "cockiness", "croquet", "crypt", "curacao", "cycle", "daiquiri", "dirndl", "disavow", "dizzying", "duplex", "dwarves", "embezzle", "equip", "espionage", "euouae", "exodus", "faking", "fishhook", "fixable", "fjord", "flapjack", "flopping", "fluffiness", "flyby", "foxglove", "frazzled", "frizzled", "fuchsia", "funny", "gabby", "galaxy", "galvanize", "gazebo", "giaour", "gizmo", "glowworm", "glyph", "gnarly", "gnostic", "gossip", "grogginess", "haiku", "haphazard", "hyphen", "iatrogenic", "icebox", "injury", "ivory", "ivy", "jackpot", "jaundice", "jawbreaker", "jaywalk", "jazziest", "jazzy", "jelly", "jigsaw", "jinx", "jiujitsu", "jockey", "jogging", "joking", "jovial", "joyful", "juicy", "jukebox", "jumbo", "kayak", "kazoo", "keyhole", "khaki", "kilobyte", "kiosk", "kitsch", "kiwifruit", "klutz", "knapsack", "larynx", "lengths", "lucky", "luxury", "lymph", "marquis", "matrix", "megahertz", "microwave", "mnemonic", "mystify", "naphtha", "nightclub", "nowadays", "numbskull", "nymph", "onyx", "ovary", "oxidize", "oxygen", "pajama", "peekaboo", "phlegm", "pixel", "pizazz", "pneumonia", "polka", "pshaw", "psyche", "puppy", "puzzling", "quartz", "queue", "quips", "quixotic", "quiz", "quizzes", "quorum", "razzmatazz", "rhubarb", "rhythm", "rickshaw", "schnapps", "scratch", "shiv", "snazzy", "sphinx", "spritz", "squawk", "staff", "strength", "strengths", "stretch", "stronghold", "stymied", "subway", "swivel", "syndrome", "thriftless", "thumbscrew", "topaz", "transcript", "transgress", "transplant", "triphthong", "twelfth", "twelfths", "unknown", "unworthy", "unzip", "uptown", "vaporize", "vixen", "vodka", "voodoo", "vortex", "voyeurism", "walkway", "waltz", "wave", "wavy", "waxy", "wellspring", "wheezy", "whiskey", "whizzing", "whomever", "wimpy", "witchcraft", "wizard", "woozy", "wristwatch", "wyvern", "xylophone", "yachtsman", "yippee", "yoked", "youthful", "yummy", "zephyr", "zigzag", "zigzagging", "zilch", "zipper", "zodiac", "zombie"]
+const char = "_ ";
+let randomWord;
+let wordLength;
+var getHint ;          // Word getHint
+var guess ;             // Geuss
+var geusses = [ ];      // Stored geusses
+var lives ;             // Lives
+var counter ;           // Count correct geusses
+
+
+
+window.onload = function () {
+    var showLives = document.getElementById("mylives"); 
+
+    const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+          'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+          't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    
+    const buttons = function () {
+      myButtons = document.getElementById('buttons');
+      letters = document.createElement('ul');
+  
+      for (var i = 0; i < alphabet.length; i++) {
+        letters.id = 'alphabet';
+        list = document.createElement('li');
+        list.id = 'letter';
+        list.innerHTML = alphabet[i];
+        check();
+        myButtons.appendChild(letters);
+        letters.appendChild(list);
+      }
+    }
+      
+  
+     result = function () {
+      randomWord = words[Math.floor(Math.random()*words.length)];
+      console.log(randomWord)
+      wordHolder = document.getElementById('word');
+      correct = document.createElement('ul');
+  
+      for (var i = 0; i < randomWord.length; i++) {
+        correct.setAttribute('id', 'my-word');
+        guess = document.createElement('li');
+        guess.setAttribute('class', 'guess');
+        guess.innerHTML = "_";
+  
+        geusses.push(guess);
+        wordHolder.appendChild(correct);
+        correct.appendChild(guess);
+      }
+    }
+    
+     comments = function () {
+      showLives.innerHTML = "You have " + lives + " lives";
+      if (lives < 1) {
+        showLives.innerHTML = "Game Over";
+      }
+      for (var i = 0; i < geusses.length; i++) {
+        if (counter + space === geusses.length) {
+          showLives.innerHTML = "You Win!";
+        }
+      }
+    }
+  
+        // Animate man
+    var animate = function () {
+      var drawMe = lives ;
+      drawArray[drawMe]();
+    }
+  
+    
+     // Hangman
+    canvas =  function(){
+  
+      myStickman = document.getElementById("stickman");
+      context = myStickman.getContext('2d');
+      context.beginPath();
+      context.strokeStyle = "#fff";
+      context.lineWidth = 2;
+    };
+    
+      head = function(){
+        myStickman = document.getElementById("stickman");
+        context = myStickman.getContext('2d');
+        context.beginPath();
+        context.arc(60, 25, 10, 0, Math.PI*2, true);
+        context.stroke();
+      }
+      
+    draw = function($pathFromx, $pathFromy, $pathTox, $pathToy) {
+      
+      context.moveTo($pathFromx, $pathFromy);
+      context.lineTo($pathTox, $pathToy);
+      context.stroke(); 
+  }
+  
+     frame1 = function() {
+       draw (0, 150, 150, 150);
+     };
+     
+     frame2 = function() {
+       draw (10, 0, 10, 600);
+     };
+    
+     frame3 = function() {
+       draw (0, 5, 70, 5);
+     };
+    
+     frame4 = function() {
+       draw (60, 5, 60, 15);
+     };
+    
+     torso = function() {
+       draw (60, 36, 60, 70);
+     };
+    
+     rightArm = function() {
+       draw (60, 46, 100, 50);
+     };
+    
+     leftArm = function() {
+       draw (60, 46, 20, 50);
+     };
+    
+     rightLeg = function() {
+       draw (60, 70, 100, 100);
+     };
+    
+     leftLeg = function() {
+       draw (60, 70, 20, 100);
+     };
+    
+    drawArray = [rightLeg, leftLeg, rightArm, leftArm,  torso,  head, frame4, frame3, frame2, frame1]; 
+  
+     check = function () {
+      list.onclick = function () {
+        var geuss = (this.innerHTML);
+        this.setAttribute("class", "active");
+        this.onclick = null;
+        for (var i = 0; i < randomWord.length; i++) {
+          if (randomWord[i] === geuss) {
+            geusses[i].innerHTML = geuss;
+            counter += 1;
+          } 
+        }
+        var j = (randomWord.indexOf(geuss));
+        if (j === -1) {
+          lives -= 1;
+          comments();
+          animate();
+        } else {
+          comments();
+        }
+      }
+    }
+    
+    play = function () {
+      buttons();
+      geusses = [ ];
+      lives = 10;
+      counter = 0;
+      space = 0;
+      result();
+      comments();
+      canvas();
+    }
+  
+    play();
+
+  
+    document.getElementById('newWord').onclick = function() {
+      correct.parentNode.removeChild(correct);
+      letters.parentNode.removeChild(letters);
+      context.clearRect(0, 0, 400, 400);
+      play();
+    }
+  }
